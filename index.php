b8a4bf696a0dce05882fd57a93e207abfb5cc7da
yazan <yazanahmadmallah@gmail.com>
2026-04-08 00:36:46 +0300

    added project structure and gitignore

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <meta charset="UTF-8">
+    <title>Multaqa Quran Portal</title>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
